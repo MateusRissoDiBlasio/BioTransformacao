5479081dcf85f0c0b0aa78b4b401be51174a25ca
unknown <mateusrisso@gmail.com>
2024-11-30 20:07:59 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import vitePluginString from 'vite-plugin-string'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import vitePluginString from 'vite-plugin-string'
 
-export default {
-  plugins: [
-    vitePluginString()
-  ]
-}
+// export default {
+//   plugins: [
+//     vitePluginString()
+//   ]
+// }
+
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/BioTransformacao/' : '/',
+  plugins: [react()],
+})
