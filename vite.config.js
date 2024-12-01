eeeea9550d8ce43b23ba5211e53fbf6f403c294f
MATEUS RISSO DI BLASIO <139410293+MateusRissoDiBlasio@users.noreply.github.com>
2024-12-01 11:18:04 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import vitePluginString from 'vite-plugin-string'
 export default {
-  base: process.env.NODE_ENV === 'production' ? '/BioTransformacao/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/BioTransformacao/' : '/',
+  base: "/BioTransformacao",
   plugins: [
     vitePluginString(),
-    base: "/BioTransformacao"
   ]
 }
