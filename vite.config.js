47694f3167c0f64c9b512ad1cc6dbaaf3ae2a8ac
unknown <mateusrisso@gmail.com>
2024-12-01 22:16:25 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import vitePluginString from 'vite-plugin-string'
 export default {
   // base: process.env.NODE_ENV === 'production' ? '/BioTransformacao/' : '/',
-  base: "/BioTransformacao/src/",
+  base: "/",
   plugins: [
     vitePluginString(),
     
