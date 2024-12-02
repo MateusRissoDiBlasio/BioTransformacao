9ce9f1934d6465e16987468c7231972c87c5bd4f
unknown <mateusrisso@yahoo.com.br>
2024-12-02 11:50:04 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import vitePluginString from 'vite-plugin-string'
+import vitePluginString from 'vite-plugin-string';
+import { rollup } from 'rollup';
+import glsl from 'rollup-plugin-glsl';
 export default {
   // base: process.env.NODE_ENV === 'production' ? '/BioTransformacao/' : '/',
   base: "/",
   plugins: [
-    vitePluginString(), glsl()    
+    vitePluginString(), 
+    rollup({entry: 'main.js',
+      plugins:[ 
+          glsl({
+            include:'lib/**/*.glsl,',
+            exclude:['**/index.html'],
+            sourceMap: false
+          })
+      ]
+})
+    
   ]
 }
