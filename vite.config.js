edfdd242b623cbac25ab966497ac34d4ee5398f4
unknown <mateusrisso@yahoo.com.br>
2024-12-02 14:26:10 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
 import vitePluginString from 'vite-plugin-string'
 import glsl from 'vite-plugin-glsl';
 
-export default defineConfig({
+export default {
   // base: process.env.NODE_ENV === 'production' ? '/BioTransformacao/' : '/',
   base: "/",
   plugins: [
     vitePluginString(),
     glsl()
   ]
-});
+};
