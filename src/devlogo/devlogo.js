90fac0209b207ba0eba416a93747a40c73b4e71e
unknown <mateusrisso@yahoo.com.br>
2024-12-11 12:41:27 -0300

    first commit

--- src/devlogo/devlogo.js
+++ src/devlogo/devlogo.js
@@ -4,13 +4,30 @@ function devlinks(){
 }
 
 
-function showhideinfo(){
+// function showhideinfo(){
     
-    var element = document.getElementById("devinfo");
-    element.classList.toggle("on");
+//     var element = document.getElementById("devinfo");
+//     element.classList.toggle("on");
 
-    var deviconcolor = document.getElementById("devi");
-    deviconcolor.classList.toggle("devcolor")
+//     var deviconcolor = document.getElementById("devi");
+//     deviconcolor.classList.toggle("devcolor")
 
-}
+// }
 
+const button = document.getElementById('deviconbtn');
+let clickCount = 0;
+var element = document.getElementById("devinfo");
+var deviconcolor = document.getElementById("devi");
+
+button.addEventListener('click', () => {
+  clickCount++;
+  if (clickCount % 2 === 1) {
+    element.classList.remove('off');
+    element.classList.add('on');
+    deviconcolor.classList.add("devcolor")
+  } else {
+    element.classList.remove('on');
+    element.classList.add('off');
+    deviconcolor.classList.remove("devcolor")
+  }
+});
